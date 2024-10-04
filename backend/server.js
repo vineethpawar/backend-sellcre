76c722cae95ef8edae3304dab1217a4c25293eda
vineethpawar <vineethpawar99@gmail.com>
2024-10-04 19:04:51 +0530

    add dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const bodyparser = require("body-parser");
 const cors = require("cors");
+
+dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
